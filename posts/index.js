df2e6c55f1030f4b210552842ce7a90d14fe8fce
duane faria <duane.faria@etec.sp.gov.br>
2021-02-07 20:35:25 -0300

    ...

--- posts/index.js
+++ posts/index.js
@@ -1,7 +1,30 @@
 const express = require("express");
-
+const { randomBytes } = require("crypto");
+const cors = require("cors");
 const app = express();
 
-app.get("/posts", () => {});
+app.use(cors());
+app.use(express.json());
 
-app.post("/posts", () => {});
+const posts = {};
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/posts", (req, res) => {
+  const id = randomBytes(4).toString("hex");
+
+  const { title } = req.body;
+
+  posts[id] = {
+    id,
+    title,
+  };
+
+  res.status(201).send(posts[id]);
+});
+
+app.listen(4000, () => {
+  console.log("listening on 4000");
+});
