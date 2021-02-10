c92251ca47034de1e2671b0743ba78388f204b50
duane faria <duane.faria@etec.sp.gov.br>
2021-02-09 22:19:07 -0300

    ...

--- comments/index.js
+++ comments/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
 const app = express();
-
+const axios = require("axios");
 app.use(express.json());
 app.use(cors());
 
@@ -12,7 +12,11 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/events", (req, res) => {
+  res.send({});
+});
+
+app.post("/posts/:id/comments", async (req, res) => {
   const commentId = randomBytes(4).toString("hex");
 
   const { content } = req.body;
@@ -22,6 +26,11 @@ app.post("/posts/:id/comments", (req, res) => {
   comments.push({ id: commentId, content });
 
   commentsByPostId[req.params.id] = comments;
+
+  await axios.post("http://localhost:4005/events", {
+    type: "CommmentCreated",
+    data: { id: commentId, content, postId: req.params.id },
+  });
 
   res.status(201).send(commentsByPostId);
 });
--- query/index.js
+++ query/index.js
@@ -14,7 +14,7 @@ app.get("/posts", (req, res) => {
 
 app.post("/events", (req, res) => {
   const { type, data } = req.body;
-
+  console.log({ type, data });
   if (type === "PostCreated") {
     const { id, title } = data;
     posts[id] = {
